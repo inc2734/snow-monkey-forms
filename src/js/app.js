9717f478af6f786c6c5aac6ea642984a563a7220
Takashi Kitajima <inc@2inc.org>
2019-04-04 20:32:34 +0900

    Some fixes

--- src/js/app.js
+++ src/js/app.js
@@ -2,60 +2,49 @@
 
 import $ from 'jquery';
 
-$( document ).on(
-	'click',
-	'[data-action="back"]',
-	function( event ) {
-		$( event.currentTarget ).parent().find( 'input[type="hidden"]' ).attr( 'value', 'back' );
-	}
+$(document).on(
+  'click',
+  '[data-action="back"]',
+  (event) => $(event.currentTarget).parent().find('input[type="hidden"]').attr('value', 'back')
 );
 
-var send = function( form ) {
-	var actionArea = form.find( '.snow-monkey-form__action' );
+const send = (form) => {
+  const actionArea = form.find('.snow-monkey-form__action');
 
-	form.on(
-		'submit',
-		function( event ) {
-			event.preventDefault();
+  form.on(
+    'submit',
+    (event) => {
+      event.preventDefault();
 
-			$.post(
-				snow_monkey_forms.view_json_url,
-				form.serialize()
-			).done(
-				function( response ) {
-					response = JSON.parse( response );
-					var method = response.data._method;
-					console.log( response );
+      $.post(
+        snow_monkey_forms.view_json_url,
+        form.serialize()
+      ).done(
+        (response) => {
+          response = JSON.parse(response);
+          const method = response.data._method;
 
-					actionArea.html( response.action );
+          console.log(response);
 
-					$.each(
-						response.controls,
-						function( key, control ) {
-							var placeholder = form.find( '.snow-monkey-form__placeholder[data-name="' + key + '"]' );
-							placeholder.html( '' );
-						}
-					);
+          actionArea.html(response.action);
 
-					if ( '' === method || 'back' === method || 'error' === method || 'confirm' === method ) {
-						$.each(
-							response.controls,
-							function( key, control ) {
-								var placeholder = form.find( '.snow-monkey-form__placeholder[data-name="' + key + '"]' );
-								placeholder.append( control );
-							}
-						);
-					} else if ( 'complete' === method ) {
-						form.html( '' ).append( response.message );
-					}
-				}
-			);
-		}
-	);
+          if ('' === method || 'back' === method || 'error' === method || 'confirm' === method) {
+            $.each(
+              response.controls,
+              (key, control) => {
+              const placeholder = form.find(`.snow-monkey-form__placeholder[data-name="${key}"]`);
+                placeholder.html( '' ).append( control );
+              }
+            );
+          } else if ('complete' === method) {
+            form.html('').append(response.message);
+          } else {
+            form.html('');
+          }
+        }
+      );
+    }
+  );
 };
 
-$( '.snow-monkey-form' ).each(
-	function( i, e ) {
-		send( $( e ) );
-	}
-);
+$('.snow-monkey-form').each((i, e) => send($(e)));
