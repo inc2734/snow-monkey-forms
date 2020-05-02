c3ac641607a65e9d181531a2aeba657a7ea31d96
Takashi Kitajima <inc@2inc.org>
2020-05-02 14:49:22 +0900

    Update packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,13 +6,4 @@ module.exports = {
 		...defaultConfig.globals,
 		snowmonkeyforms: true,
 	},
-	rules: {
-		...defaultConfig.rules,
-		'@wordpress/i18n-text-domain': [
-			'error',
-			{
-				allowedTextDomain: 'snow-monkey-forms',
-			},
-		],
-	},
 };
